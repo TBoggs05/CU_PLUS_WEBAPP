3f864e228b23537259b0cb68860198c7e378af1d
VorahPong <vorahpongmean@gmail.com>
2025-12-23 21:04:05 -0600

    Added first page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'features/auth/ui/login_page.dart';
+import 'features/auth/ui/first_page.dart';
 
 void main() => runApp(const MyApp());
 
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily: 'DMSans'),
-      home: LoginPage(),
+      home: FirstPage(),
     );
   }
 }
