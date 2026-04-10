e58818c838ad70dab471767ac6ecdf473ef4dd46
solistis <solistis01@gmail.com>
2026-04-10 13:48:10 -0500

    Push course_content progress (Not finished)

--- lib/features/admin/ui/course_content_view.dart
+++ lib/features/admin/ui/course_content_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../widgets/folder.dart';
 
 class CourseContentView extends StatefulWidget {
   const CourseContentView({
@@ -14,6 +15,95 @@ class CourseContentView extends StatefulWidget {
 
 class _CourseContentViewState extends State<CourseContentView> {
   bool isEditMode = true;
+
+  List<Folder> folders = [
+  Folder(title: "1st Year PLUS Materials"),
+  Folder(title: "2nd Year PLUS Materials"),
+  Folder(title: "3rd & 4th Year PLUS Materials"),
+  Folder(title: "Volunteer Opportunities"),
+  Folder(title: "Career Profiling Tools"),
+  ];
+
+  Widget _buildFolderTile(Folder folder, int index) {
+  return Container(
+    margin: const EdgeInsets.only(bottom: 16),
+    padding: const EdgeInsets.all(16),
+    decoration: BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.circular(12),
+      boxShadow: [
+        BoxShadow(
+          color: Colors.black12,
+          blurRadius: 4,
+          offset: Offset(0, 2),
+        ),
+      ],
+    ),
+    child: Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              folder.title,
+              style: const TextStyle(fontSize: 16),
+            ),
+
+            Row(
+              children: [
+                IconButton(
+                  icon: const Icon(Icons.edit_outlined),
+                  onPressed: () {},
+                ),
+
+                OutlinedButton.icon(
+                  onPressed: () {
+                    // later: add subfolder/content
+                  },
+                  icon: const Icon(Icons.add),
+                  label: const Text("Add Content"),
+                ),
+
+                IconButton(
+                  icon: Icon(
+                    folder.isExpanded
+                        ? Icons.keyboard_arrow_up
+                        : Icons.keyboard_arrow_down,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      folder.isExpanded = !folder.isExpanded;
+                    });
+                  },
+                ),
+
+                IconButton(
+                  icon: const Icon(Icons.delete, color: Colors.red),
+                  onPressed: () {
+                    setState(() {
+                      folders.removeAt(index);
+                    });
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
+
+        // 👇 Expanded content (subfolders)
+        if (folder.isExpanded)
+          Column(
+            children: folder.children.map((child) {
+              return ListTile(
+                leading: const Icon(Icons.folder_outlined),
+                title: Text(child.title),
+              );
+            }).toList(),
+          ),
+      ],
+    ),
+  );
+}
 
   @override
   Widget build(BuildContext context) {
@@ -67,7 +157,11 @@ class _CourseContentViewState extends State<CourseContentView> {
 
               OutlinedButton.icon(
                 onPressed: () {
-                  // functionality will be added later
+                  setState(() {
+                    folders.add(
+                      Folder(title: "New Folder"),
+                    );
+                  });
                 },
                 icon: const Icon(Icons.add_circle_outline),
                 label: const Text("Create new folder"),
@@ -93,15 +187,12 @@ class _CourseContentViewState extends State<CourseContentView> {
                 color: Colors.grey.shade100,
                 borderRadius: BorderRadius.circular(12),
               ),
-              child: const Center(
-                child: Text(
-                  "Empty",
-                  style: TextStyle(
-                    fontSize: 40,
-                    fontStyle: FontStyle.italic,
-                    color: Colors.grey,
-                  ),
-                ),
+              child: ListView.builder(
+                  itemCount: folders.length,
+                  itemBuilder: (context, index) {
+                    final folder = folders[index];
+                    return _buildFolderTile(folder, index);
+                  },
               ),
             ),
           ),
