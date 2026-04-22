2af3b0d3b8b5fc0e5006b114ec3a6f2ab479f5e2
VorahPong <vorahpongmean@gmail.com>
2026-04-22 11:12:20 -0500

    Harded code backend url

--- lib/core/config/api_config.dart
+++ lib/core/config/api_config.dart
@@ -1,6 +1,7 @@
 class ApiConfig {
   static const String baseUrl = String.fromEnvironment(
     'API_BASE_URL',
-    defaultValue: 'http://localhost:4000',
+    // defaultValue: 'http://localhost:4000',
+    defaultValue: 'http://cuplus-backend-env.eba-sufmkysk.us-east-2.elasticbeanstalk.com',
   );
 }
